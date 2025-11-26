5232c36f7d29fe4ae85242af306efde860477a9e
Thomas Kottke <t.kottke90@gmail.com>
2025-11-25 20:47:35 -0600

    refactor: add additional keyboard command handlers

--- src/lib/events/keyboard.ts
+++ src/lib/events/keyboard.ts
@@ -1,8 +1,39 @@
+
+export function CommandHandler<TEvent extends KeyboardEvent>(
+  callback: (event: TEvent) => void | Promise<void>
+) {
+  return (event: TEvent) => {
+    if (event.key === "p" && event.ctrlKey) {
+      callback(event);
+    }
+  };
+}
+
 export function EnterHandler<TEvent extends KeyboardEvent>(
   callback: (event: TEvent) => void | Promise<void>
 ) {
   return (event: TEvent) => {
     if (event.key === "Enter" && !event.shiftKey) {
+      callback(event);
+    }
+  };
+}
+
+export function NewHandler<TEvent extends KeyboardEvent>(
+  callback: (event: TEvent) => void | Promise<void>
+) {
+  return (event: TEvent) => {
+    if (event.key === "n" && event.ctrlKey) {
+      callback(event);
+    }
+  };
+}
+
+export function SaveHandler<TEvent extends KeyboardEvent>(
+  callback: (event: TEvent) => void | Promise<void>
+) {
+  return (event: TEvent) => {
+    if (event.key === "s" && event.ctrlKey) {
       callback(event);
     }
   };
